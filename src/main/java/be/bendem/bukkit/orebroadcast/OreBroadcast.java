c58b79cb8c2f417dab159faf7335ff6067332900
Siroshun09 <siro.shun8@gmail.com>
2019-09-14 06:40:45 +0900

    [Add] Config クラス削除に伴う修正

--- src/main/java/be/bendem/bukkit/orebroadcast/OreBroadcast.java
+++ src/main/java/be/bendem/bukkit/orebroadcast/OreBroadcast.java
@@ -6,40 +6,42 @@ import org.bukkit.block.Block;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class OreBroadcast extends JavaPlugin {
 
-    private Config config;
+    private final Set<SafeBlock> broadcastBlacklist = new HashSet<>();
+    private final Set<String> disableWorlds = new HashSet<>();
 
     @Override
     public void onEnable() {
-        config = new Config(this);
-        config.loadConfig();
+        saveDefaultConfig();
+        reloadConfig();
+
+        disableWorlds.addAll(getConfig().getStringList("disableWorlds"));
 
         getServer().getPluginManager().registerEvents(new BlockBreakListener(this), this);
         getServer().getPluginManager().registerEvents(new BlockPlaceListener(this), this);
     }
 
     public void blackList(Block block) {
-        config.getBroadcastBlacklist().add(new SafeBlock(block));
+        broadcastBlacklist.add(new SafeBlock(block));
     }
 
     public void blackList(Collection<Block> blocks) {
-        for(Block block : blocks) {
-            blackList(block);
-        }
+        for (Block block : blocks) blackList(block);
     }
 
     public void unBlackList(Block block) {
-        config.getBroadcastBlacklist().remove(new SafeBlock(block));
+        broadcastBlacklist.remove(new SafeBlock(block));
     }
 
     public boolean isBlackListed(Block block) {
-        return config.getBroadcastBlacklist().contains(new SafeBlock(block));
+        return broadcastBlacklist.contains(new SafeBlock(block));
     }
 
     public boolean isWorldDisabled(String world) {
-        return config.getDisableWorlds().contains(world);
+        return disableWorlds.contains(world);
     }
-
 }
